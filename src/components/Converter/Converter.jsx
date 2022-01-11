03b90643ffee9abf8fc1cf291dd411e660869acd
ghsdl <maeva.ghsdl@gmail.com>
2022-01-11 10:35:18 +0100

    :pushpin: test toggle before Toggler component
    useState for toggle

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header.jsx';
 import Currencies from '../Currencies/Currencies.jsx';
 import Amount from '../Amount/Amount.jsx';
@@ -7,10 +7,14 @@ import './Converter.scss';
 import currencies from '../../data/currencies.js';
 
 const Converter = () => {
+  const [toggle, setToggle] = useState(null);
   return (
     <div className='converter'>
       <Header value={5} />
-      <Currencies currencies={currencies} />
+      <button type='button' onClick={() => setToggle(!toggle)}>
+        Show currencies
+      </button>
+      {toggle && <Currencies currencies={currencies} />}
       <Amount value={1.09} currency='Pound' />
     </div>
   );
